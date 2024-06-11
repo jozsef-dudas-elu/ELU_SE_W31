fbdd57b40c714179a3072a303a1dfb4afe162918
Joe <jozsef.dudas@elu.nl>
2024-06-11 14:03:44 +0200

    Fix errors repored by linter

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,12 +1,13 @@
 def CalculateTotal(cart):
     total = 0
-    total_items = len(cart)
     for item in cart:
         total += item['price']
     return total
 
+
 def display_total(Total):
     print("Total price: " + Total)
+
 
 CART = [
     {'name': 'Item A', 'price': 10.99},
@@ -19,4 +20,3 @@ for item in CART:
 
 shopping_cart_total = CalculateTotal(CART)
 display_total(shopping_cart_total)
-
